127de29b0a21deb49cb2d4d0b4e459508d4f988e
kirjai <9858620+kirjai@users.noreply.github.com>
2025-08-15 09:33:23 +0100

    drop devtools

--- src/presentation/routes/root.route.tsx
+++ src/presentation/routes/root.route.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { QueryClientProvider } from "../query-client/query-client";
 
 export const rootRoute = createRootRoute({
@@ -8,7 +7,6 @@ export const rootRoute = createRootRoute({
 			<QueryClientProvider>
 				<Outlet />
 			</QueryClientProvider>
-			<TanStackRouterDevtools />
 		</>
 	),
 });
